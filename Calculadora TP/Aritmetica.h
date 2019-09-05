1ee37b7eb266620cd4dea86403d4c0815de1ef82
Franco52001 <52410416+Franco52001@users.noreply.github.com>
2019-09-05 13:41:47 -0300

    Add files via upload

---
+++ Calculadora TP/Aritmetica.h
@@ -0,0 +1,64 @@
+#include <stdio.h>
+/** \brief Pide numero
+ *
+ * \param char[]Mensaje transmitido a la hora de pedir el numero
+ * \return float Tipo de numero que pide
+ *
+ */
+float pedirNum(char[]);
+/** \brief Es la estructura de la calculadora
+ *
+ * \param numUno float Primer numero utilizado
+ * \param numDos float Segundo numero utilizado
+ * \return void
+ *
+ */
+void  calcular(float numUno,float numDos);
+/** \brief Realiza la operacion suma
+ *
+ * \param numUno float Primer numero utilizado en la suma
+ * \param numDos float Segundo numero utilizado en la suma
+ * \return float
+ *
+ */
+float sumar(float numUno, float numDos);
+/** \brief Realiza la operacion resta
+ *
+ * \param numUno float Primer numero utilizado en la resta
+ * \param numDos float Segundo numero utilizado en la resta
+ * \return float
+ *
+ */
+float restar(float numUno, float numDos);
+/** \brief Realiza la operacion multiplicacion
+ *
+ * \param numUno float primer numero utilizado en la multiplicacion
+ * \param numDos float Segundo numero utilizado en la multiplicacion
+ * \return float
+ *
+ */
+float multiplicar(float numUno, float numDos);
+/** \brief Realiza la operacion division
+ *
+ * \param numUno float Primer numero utilizado en la division
+ * \param numDos float Segundo numero utilizado en la division
+ * \return float
+ *
+ */
+float dividir(float numUno, float numDos);
+/** \brief Realiza el factorial del primer numero
+ *
+ * \param numUno float Primer numero al cual se le aplicara la funcion factorial
+ * \return float
+ *
+ */
+float factorialUno(float numUno);
+/** \brief Realiza la operacion del segundo numero
+ *
+ * \param numDos float Segundo numero al cual se le aplicara la funcion factorial
+ * \return float
+ *
+ */
+float factorialDos(float numDos);
+
+
---
+++ Calculadora TP/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "Aritmetica.h"
+
+int main()
+{
+    float numUno = 0;
+    float numDos = 0;
+
+    calcular(numUno,numDos);
+
+}
+
